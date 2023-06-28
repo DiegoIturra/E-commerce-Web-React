50d5d91308065da60f7e73540242f54ae0c3145d
Joselois1 <sfxjose@gmail.com>
2023-06-28 13:52:38 -0400

    quitar tallas

--- src/modules/Product/index.js
+++ src/modules/Product/index.js
@@ -43,6 +43,7 @@ const Product = () => {
 
   if(!Object.keys(product).length > 0) return <div>Loading.....</div>
   
+  
   return (
     <section className="text-gray-600 body-font overflow-hidden bg-gray-900">
       <div className="container px-5 py-24 mx-auto">
@@ -166,14 +167,7 @@ const Product = () => {
                 <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
               </div>
               <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
                 <div className="relative">
-                  <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
                   <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                     <svg
                       fill="none"
